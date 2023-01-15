510ad4ccea4ba3a7bfcc31212bfb152453296edf
Mrider007 <122354876+Mrider007@users.noreply.github.com>
2023-01-15 20:13:12 +0530

    dsa
    Dsa problem

---
+++ dsa1/dsa_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.*;
+
+class dsa_1{
+    public static void main(String[] args) {
+        // for( int i = 0; i<11;i++){
+        //     System.out.println(i);
+        // }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //int sum = 0;
+        
+        // for(int i=1;i<n;i++){
+        //     sum = n+sum;
+        // }
+        // System.out.println(sum);
+        int i = 1;
+        for(i=1;i<=10;i++){
+            System.out.println(n*i);
+       }
+
+
+    }
+}
